e45631ea6c1db3438022e6c910d7429ef65a5028
lordu98 <u.makhmudov211326@gmail.com>
2022-07-02 23:38:16 +0500

    Initial commit

---
+++ src/main/java/com/example/first_level_task1/FirstLevelTask1Application.java
@@ -0,0 +1,13 @@
+package com.example.first_level_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstLevelTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstLevelTask1Application.class, args);
+    }
+
+}
